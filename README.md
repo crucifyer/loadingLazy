98e9453ad4b391e5ea840a53c2d20efa2d0073b8
crucify <shj@xenosi.de>
2024-11-10 22:11:17 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # loadingLazy
 loading=lazy for background-image
+
+declare in head
+```css
+[loading="lazy"]:not(img,iframe) { background-image: none !important; }
+```
+additional declarations for the fade in effect
+```css
+@keyframes __lazyFadeIn__ {
+    from { opacity: 0; }
+    to { opacity: 1; }
+}
+.__loading_lazy__ {
+    opacity: 0;
+    animation: __lazyFadeIn__ 1s ease-in-out forwards;
+}
+```
+
+loading js at the bottom
+```html
+<script src="https://crucifyer.github.io/js/loadingLazy.min.js"></script>
+</body>
+```
+
+example https://framework.xenosi.de/exams/loadingLazy.html
---
+++ loadingLazy.js
@@ -0,0 +1,37 @@
+/*
+ Project : https://github.com/crucifyer/loadingLazy
+ License : MIT
+*/
+(function(d) {
+	observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.removeAttribute('loading'); // 화면내에 들어오면 css 규칙에서 제외
+					entry.target.classList.add('__loading_lazy__'); // fade in 효과
+					observer.unobserve(entry.target); // 한번 보이면 더이상의 관찰 불필요
+				}
+			}, {
+				root: null,
+				rootMargin: "0px",
+				threshold: 0, // 1px 라도 보이면 동작. 1 은 다 보여야 동작
+			});
+		});
+
+	const mutationObserver = new MutationObserver((mutations) => {
+		mutations.forEach(mutation => {
+			mutation.addedNodes.forEach(node => {
+				if (node.nodeType === 1 && node.tagName !== 'IMG' && node.tagName !== 'IFRAME') {
+					if(node.loading === 'lazy') observer.observe(node); // 새로 추가되는 객체도 관찰
+					node.querySelectorAll('[loading=lazy]:not(img,iframe)').forEach(node => observer.observe(node));
+				}
+			});
+		});
+	});
+	mutationObserver.observe(d.body, {
+		childList: true,
+		subtree: true
+	});
+
+	d.querySelectorAll('[loading=lazy]:not(img,iframe)').forEach(node => observer.observe(node)); // 동적 추가되지 않은 객체 한번에 처리를 위해 /body 바로 위에 위치.
+})(document);
